6583081e943e7544aa726450e8b6298e08581477
m2lovric <m2lovric@gmail.com>
2024-07-17 22:05:30 +0200

    Add filter by status feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
+import { useState } from 'react';
 import AddIssueForm from './AddIssueForm';
 import Issue from './components/Issue';
 
 import { useGetIssues } from './hooks/useGetIssues';
+import { TIssue } from './types';
 
 function App() {
   const { data, isLoading } = useGetIssues();
+  const [filteredData, setFilteredData] = useState<TIssue[]>();
+  const [filter, setFilter] = useState(false);
+  const [filterStatus, setFilterStatus] = useState('');
+
+  function filterByStatus() {
+    setFilteredData(data?.filter((issue) => issue.status == filterStatus));
+    setFilter(true);
+  }
+
+  function resetFilter() {
+    setFilter(false);
+  }
 
   if (isLoading) return <div>Loading...</div>;
 
   return (
     <section>
       <AddIssueForm />
+      <section className='flex flex-col'>
+        <p>Filter by status</p>
+        <select onChange={(e) => setFilterStatus(e.target.value)}>
+          <option value={'LOW'}>LOW</option>
+          <option value={'MEDIUM'}>MEDIUM</option>
+          <option value={'HIGH'}>HIGH</option>
+        </select>
+
+        <button onClick={filterByStatus}>Filter</button>
+        <button onClick={resetFilter}>Reset</button>
+      </section>
       <section>
-        {data &&
-          data
-            .sort((a, b) => {
-              return (
-                new Date(b.createdAt).getTime() -
-                new Date(a.createdAt).getTime()
-              );
-            })
-            .map((issue) => <Issue key={issue.id.toString()} issue={issue} />)}
+        {!filter
+          ? data &&
+            data
+              .sort((a, b) => {
+                return (
+                  new Date(b.createdAt).getTime() -
+                  new Date(a.createdAt).getTime()
+                );
+              })
+              .map((issue) => <Issue key={issue.id.toString()} issue={issue} />)
+          : filteredData &&
+            filteredData.map((el) => (
+              <Issue key={el.id.toString()} issue={el} />
+            ))}
       </section>
     </section>
   );
