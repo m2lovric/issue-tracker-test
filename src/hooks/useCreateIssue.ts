2e57bd976f291d0208e4384074de3d27233c9619
m2lovric <m2lovric@gmail.com>
2024-07-27 22:23:02 +0200

    Fix set and get data

--- src/hooks/useCreateIssue.ts
+++ src/hooks/useCreateIssue.ts
@@ -1,14 +1,19 @@
 import { useQueryClient, useMutation } from '@tanstack/react-query';
-import { TIssue } from '../types';
+import { Status, TIssue } from '../types';
 
 function useCreateIssue() {
   const queryClient = useQueryClient();
 
   const { mutate } = useMutation({
-    mutationFn: async (bodyData: { title: string }): Promise<TIssue> => {
+    mutationFn: async (bodyData: {
+      title: string;
+      status: Status;
+      userId: string;
+    }): Promise<TIssue> => {
       const response = await fetch('http://localhost:4001/issues/', {
         method: 'POST',
         body: JSON.stringify(bodyData),
+        credentials: 'include',
         headers: {
           'Content-Type': 'application/json',
         },
--- src/hooks/useGetIssues.ts
+++ src/hooks/useGetIssues.ts
@@ -5,11 +5,14 @@ function useGetIssues() {
   const { data, error, isLoading } = useQuery({
     queryKey: ['issues'],
     queryFn: async (): Promise<TIssue[]> => {
+      const localData = JSON.parse(localStorage.getItem('user') as string);
+      console.log(localData.id);
       const response = await fetch('http://localhost:4001/issues/', {
         method: 'GET',
         credentials: 'include',
         headers: {
           'Content-Type': 'application/json',
+          ['userId']: localData.id,
         },
       });
       if (!response.ok) throw new Error('Fetch error');
